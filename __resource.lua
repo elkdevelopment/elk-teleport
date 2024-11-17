9b78e6d1ded4defadf0939b728e77049939a8371
elkdevelopment <elkshmelk@gmail.com>
2024-11-17 15:06:19 -0800

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 lua54 'yes'
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 author 'elk'
-description 'Simple Teleport Script'
+description 'Simple Teleport Script - Edited'
 
 client_scripts {
   'config.lua',
